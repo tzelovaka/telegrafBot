71031384bf72280a6c579e8785535dccb3def601
Daria <Tr3deprinter@yandex.ru>
2022-07-02 22:42:36 +0300

    qwedf

--- db.js
+++ db.js
@@ -1,16 +1,11 @@
 const {Sequelize} = require('sequelize')
-module.exports = new Sequelize(
-    'da3dj75k47iina',
-    'uumihhewuscsaz',
-    'bc8deae462f543e0c6692197da02cfc5e68bc0e1f774b7cacd8c9bca473825e8',
-    {
-        host: '72.44.34.25',
-        port: '5432',
-        dialect: 'postgres',
-        protocol: 'postgres',
-        dialectOptions: {
-            ssl: true,
+module.exports = sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
             rejectUnauthorized: false
         }
     }
-)
+});

35d907e371acf916f919382f3f32efea9e6ca80a
Daria <Tr3deprinter@yandex.ru>
2022-07-04 22:25:18 +0300

    qdf

--- model.js
+++ model.js
@@ -2,7 +2,7 @@ const sequelize = require('./db')
 const {DataTypes} = require('sequelize')
 
 const bmw = sequelize.define ('bmw', {
-    id: {type: DataTypes.STRING, primaryKey: true, unique: true, autoIncrement: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
     model: {type: DataTypes.STRING, allowNull: false},
     pic: {type: DataTypes.STRING, allowNull: false},
 })

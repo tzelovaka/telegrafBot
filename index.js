e1681c8e5fdbf012e62c02047bac0c70e6b0186b
Daria <Tr3deprinter@yandex.ru>
2022-07-05 14:42:54 +0300

    qdf

--- index.js
+++ index.js
@@ -62,17 +62,14 @@ bot.command ('add', async (ctx) => ctx.scene.enter('sceneWizard'))
 
 const Rdata = new Composer()
 Rdata.on ('text', async (ctx)=>{
-  let i = 1;
-  for (; ;){
+  for (let i = 1; i > 15; i++){
   const query = await bmw.findByPk(i).then(async bmw=>{
       if(!bmw) return;
       //await ctx.reply (`${bmw.pic}`);
       //console.log(query);
   }).catch(err=>console.log(err));
-  var x = `${bmw.model}`;
-  if(x.length < 1) break;
+  if (`${bmw.model}`.length < 1) break;
   await ctx.reply (`${bmw.model}`);
-  i++
 }
   return ctx.scene.leave()
 })

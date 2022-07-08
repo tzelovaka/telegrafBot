d317f64f383fec6b76ff0ba761ae790d166399f8
Daria <Tr3deprinter@yandex.ru>
2022-07-08 16:42:57 +0300

    qdf

--- index.js
+++ index.js
@@ -47,16 +47,11 @@ carPic.on ('text', async (ctx)=>{
   ctx.wizard.state.data.carPic = ctx.message.text;
   console.log(`${ctx.wizard.state.data.carMod}`)
   console.log(`${ctx.wizard.state.data.carPic}`)
-  try{
   const query = await bmw.create({
     model: `${ctx.wizard.state.data.carMod}`,
     pic: `${ctx.wizard.state.data.carPic}`
   }, { transaction: t });
   await t.commit();
-} catch (error) {
-  await t.rollback();
-
-}
   return ctx.scene.leave()
 })
 

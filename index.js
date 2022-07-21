61519e8923999804e17d930745bf1834510d515d
Daria <Tr3deprinter@yandex.ru>
2022-07-21 19:57:04 +0300

    qdf

--- index.js
+++ index.js
@@ -207,8 +207,8 @@ bot.use(stager.middleware())
 bot.command ('block', async (ctx) => ctx.scene.enter('sceneBlock'))
 
 bot.command ('play', async (ctx) => {
-  const play = await storybl.findOne({where: {linid: 0}});
-  await ctx.reply(`${storybl.bl}`)
+  const row = await storybl.findOne({where: {linid: 0}});
+  await ctx.reply(`${row.bl}`)
 })
 
 bot.launch()

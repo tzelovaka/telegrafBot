f3c4132f6b6ba0562596b07813784fea06bea6e8
Daria <Tr3deprinter@yandex.ru>
2022-07-25 20:37:47 +0300

    qdf

--- index.js
+++ index.js
@@ -107,6 +107,7 @@ bot.action(flagBtn.filter({action: 'true'}), async (ctx)=>{
 
 const blockChoice = new Composer()
 blockChoice.on ('callback_query', async (ctx)=>{
+  console.log(ctx.callbackQuery.data.number);
   ctx.wizard.state.data.blockChoice = ctx.callbackQuery.data.number;//ctx.message.text;
   await ctx.reply ('Введите текст ссылки.');
   

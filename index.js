550da8107158fcf6fc518b439acfcab12d31e606
Daria <Tr3deprinter@yandex.ru>
2022-07-25 20:35:00 +0300

    qdf

--- index.js
+++ index.js
@@ -98,18 +98,16 @@ ctx.wizard.state.data = {};
   console.log(e);
   await ctx.replyWithHTML('<i>Ошибка!</i>')
 }
-var f;
 bot.action(flagBtn.filter({action: 'true'}), async (ctx)=>{
   const { number, action } = flagBtn.parse(ctx.callbackQuery.data);
-  f = number;
+  console.log(number);
 })
   return ctx.wizard.next()
 })
 
 const blockChoice = new Composer()
 blockChoice.on ('callback_query', async (ctx)=>{
-  ctx.wizard.state.data.blockChoice = f;//ctx.message.text;
-  f = 0;
+  ctx.wizard.state.data.blockChoice = ctx.callbackQuery.data.number;//ctx.message.text;
   await ctx.reply ('Введите текст ссылки.');
   
   return ctx.wizard.next()

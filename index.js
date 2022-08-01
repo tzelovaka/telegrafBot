84a5a930db9a2ca9e903de83d1fb45d0f08b80eb
Daria <Tr3deprinter@yandex.ru>
2022-08-01 17:21:59 +0300

    qdf

--- index.js
+++ index.js
@@ -291,17 +291,18 @@ bot.command ('play', async (ctx) => {
   await ctx.reply (`${row.desc}`)
   var p = 0; //linid
   var r = row.id
+  var ctxid = `${ctx.message.from.id}`;
   btnLoop();
   async function btnLoop() {
   const row = await storybl.findOne({where: {
     linid: p,
     storyId: r,
-    authId: `${ctx.message.from.id}`,
+    authId: ctxid,
     release: false
   }
 });
   const {count, rows} = await storylin.findAndCountAll ({where: {
-    authId: `${ctx.message.from.id}`,
+    authId: ctxid,
     release: false,
     storyblId: `${row.id}`
   }});
@@ -321,6 +322,7 @@ bot.command ('play', async (ctx) => {
 bot.action(flagBtn.filter({action: 'true'}), async (ctx)=>{
   const { number, action } = flagBtn.parse(ctx.callbackQuery.data);
   p = number
+
   btnLoop();
 })
 } catch (e){

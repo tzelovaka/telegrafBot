905f89249189176f4b420b677d729d270b2b949a
Daria <Tr3deprinter@yandex.ru>
2022-08-05 19:28:33 +0300

    qdf

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if (BOT_TOKEN === undefined) {
 
 try {
   sequelize.authenticate()
-  //sequelize.sync({ force: true })
+  sequelize.sync({ force: true })
   console.log('Соединение с БД было успешно установлено.')
 } catch (e) {
   console.log('Невозможно выполнить подключение к БД ', e)
@@ -60,7 +60,7 @@ storyDesc.on ('text', async (ctx)=>{
     const query = await story.create({
     name: `${ctx.wizard.state.data.storyName}`,
     desc: `${ctx.wizard.state.data.storyDesc}`,
-    authId: `${ctx.message.from.id}`,
+    authId: ctx.message.from.id,
     release: false
   }, { transaction: t });
 })
@@ -77,14 +77,14 @@ baseSave.on ('text', async (ctx)=>{
   const t = await sequelize.transaction();
   try{
     const { count, rows } = await story.findAndCountAll({where: {
-      authId: ctx.message.from.id,//`${ctx.message.from.id}`,
+      authId: ctx.message.from.id,
       release: false}});
     let c = count - 1;
     const result = await sequelize.transaction(async (t) => {
     const query = await storybl.create({
     linid: 0,
     bl: `${ctx.wizard.state.data.baseSave}`,
-    authId: `${ctx.message.from.id}`,//`${ctx.message.from.id}`,
+    authId: ctx.message.from.id,
     storyId: rows[c].id,
     release: false
   }, { transaction: t });
@@ -116,8 +116,6 @@ const blockEmpty = new Composer()
 blockEmpty.on ('text', async (ctx)=>{
 ctx.wizard.state.data = {};
 try{
-//const {cou, row} = await story.findAndCountAll({where: {authId: ctx.message.from.id}});
- // var n = cou - 1;
   const { count, rows } = await storybl.findAndCountAll({where: {
     authId: ctx.message.from.id,
     release: false

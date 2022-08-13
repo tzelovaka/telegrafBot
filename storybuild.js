a4d9b82d2e5cb6190b629b294071cfd9980d90d2
Daria <Tr3deprinter@yandex.ru>
2022-08-13 15:19:37 +0300

    qdf

---
+++ storybuild.js
@@ -0,0 +1,87 @@
+const baseEmpty = new Composer()
+baseEmpty.on ('text', async (ctx)=>{
+  ctx.wizard.state.data = {};
+  const count = await story.count({where: {
+    authId: ctx.message.from.id, 
+    release: false,
+  }});
+  if (count > 0) {
+    await ctx.reply ('История уже создаётся!');
+    return ctx.scene.leave()
+  }
+  await ctx.reply('Введите название.', Markup.keyboard(
+    [
+    ['🔙Выйти']
+  ]))
+  return ctx.wizard.next()
+})
+
+const storyName = new Composer()
+storyName.on ('text', async (ctx)=>{
+  if (ctx.message.text === '🔙Выйти') 
+  {
+    await ctx.reply ('Операция прошла успешно.');
+    return ctx.scene.leave()
+  }
+  ctx.wizard.state.data.storyName = ctx.message.text;
+  await ctx.reply ('Введите описание истории');
+  return ctx.wizard.next()
+})
+
+const storyDesc = new Composer()
+storyDesc.on ('text', async (ctx)=>{
+  if (ctx.message.text === '🔙Выйти') 
+  {
+    await ctx.reply ('Операция прошла успешно.');
+    return ctx.scene.leave()
+  }
+  ctx.wizard.state.data.storyDesc = ctx.message.text;
+  await ctx.reply ('Введите текст открывающего блока (блок, за которым последует первый выбор).');
+  const t = await sequelize.transaction();
+  try{
+    const result = await sequelize.transaction(async (t) => {
+    const query = await story.create({
+    name: `${ctx.wizard.state.data.storyName}`,
+    desc: `${ctx.wizard.state.data.storyDesc}`,
+    authId: ctx.message.from.id,
+    release: false
+  }, { transaction: t });
+})
+await t.commit('commit');
+} catch (error) {
+  await t.rollback();
+}
+  return ctx.wizard.next()
+})
+
+const baseSave = new Composer()
+baseSave.on ('text', async (ctx)=>{
+  ctx.wizard.state.data.baseSave = ctx.message.text;
+  const t = await sequelize.transaction();
+  try{
+    const { count, rows } = await story.findAndCountAll({where: {
+      authId: ctx.message.from.id,
+      release: false}});
+    let c = count - 1;
+    const result = await sequelize.transaction(async (t) => {
+    const query = await storybl.create({
+    linid: 0,
+    bl: `${ctx.wizard.state.data.baseSave}`,
+    authId: ctx.message.from.id,
+    storyId: rows[c].id,
+    release: false
+  }, { transaction: t });
+})
+await t.commit('commit');
+} catch (error) {
+  await t.rollback();
+}
+  await ctx.reply ('Вы успешно добавили первый блок своей будущей истории.');
+  return ctx.scene.leave()
+})
+
+
+const menuCreate = new Scenes.WizardScene('sceneCreate', baseEmpty, storyName, storyDesc, baseSave)
+const stage = new Scenes.Stage ([menuCreate])
+bot.use(session())
+bot.use(stage.middleware())

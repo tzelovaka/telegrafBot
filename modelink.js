cd93520d76e595af196b10affac3f01411b6b245
Daria <Tr3deprinter@yandex.ru>
2022-08-14 23:19:38 +0300

    qdf

--- modelink.js
+++ modelink.js
@@ -4,7 +4,7 @@ const {DataTypes} = require('sequelize')
 const storylin = sequelize.define ('storylin', {
     id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
     link: {type: DataTypes.STRING},
-    smile: {type: DataTypes.STRING(1), allowNull: true, defaultValue: null},
+    smile: {type: DataTypes.STRING(1), allowNull: true, defaultValue: '👆'},
     authId: {type: DataTypes.BIGINT},
     release: {type: DataTypes.BOOLEAN, defaultValue: false}
 })

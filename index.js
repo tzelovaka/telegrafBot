fe0ebd8c742696dd28612d01660bc5e696ee5423
Daria <Tr3deprinter@yandex.ru>
2022-08-17 21:20:36 +0300

    qdf

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ bot.command ('link', async (ctx) => ctx.scene.enter('sceneLink'))
 
 
 
-const linkBtn = new CallbackData('linkBtn', ['id', 'smile', 'storyblid', 'storyid']);
+const linkBtn = new CallbackData('linkBtn', ['id', 'action'/*'smile', 'storyblid', 'storyid'*/]);
 const linkEmpty = new Composer()
 linkEmpty.on ('text', async (ctx)=>{
   try{
@@ -260,15 +260,15 @@ linkEmpty.on ('text', async (ctx)=>{
         linid: rows[i].id,
         storyId: row.id
       }})
-      console.log(ro);
       if (ro === null){
       await ctx.reply(`${rows[i].link}`, Markup.inlineKeyboard(
         [
         [Markup.button.callback(`${rows[i].smile}`, linkBtn.create({
           id: rows[i].id,
-          smile: rows[i].smile,
-          storyblid: rows[i].storyblId,
-          storyid: rows[i].storyId
+          action: 'true',
+          //smile: rows[i].smile,
+          //storyblid: rows[i].storyblId,
+          //storyid: rows[i].storyId
         }))]
           ]
           )
@@ -290,17 +290,7 @@ linkEmpty.on ('text', async (ctx)=>{
 const linkChoice = new Composer()
 linkChoice.on ('callback_query', async (ctx)=>{
   try{
-  const { id, smile, storyblid, storyid} = linkBtn.parse(ctx.callbackQuery.data);
-  const row = await storylin.findOne({where:{
-    id: id,
-    smile: smile,
-    storyblId: storyblid,
-    storyId: storyid
-  }})
-  if (row === null) {
-    await ctx.answerCbQuery('Ошибка! Такой ссылки нет в базе данных!⚠');
-    return ctx.scene.leave()
-  }
+  const { id, action} = linkBtn.parse(ctx.callbackQuery.data);
   ctx.wizard.state.data.linkChoice = id;
   const count = await storybl.count({where: {
     linid: ctx.wizard.state.data.linkChoice,
@@ -308,7 +298,7 @@ linkChoice.on ('callback_query', async (ctx)=>{
     release: false
   }});
   if (count > 0){
-    await ctx.answerCbQuery('Ошибка! Ссылка уже ведёт к одному из блоков!⚠')
+    await ctx.answerCbQuery('Ошибка!⚠')
     return ctx.scene.leave()
   }
 } catch(e){

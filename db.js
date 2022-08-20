cd04d7e4c1d965e8a0bc47ad264a71ed04bd9e5e
Daria <Tr3deprinter@yandex.ru>
2022-08-20 14:18:24 +0300

    qdf

--- db.js
+++ db.js
@@ -1,4 +1,8 @@
 const {Sequelize} = require('sequelize')
+const cls = require('cls-hooked');
+const namespace = cls.createNamespace('my-very-own-namespace');
+Sequelize.useCLS(namespace);
+
 module.exports = sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_OLIVE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',

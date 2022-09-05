f74a23851a15f1040cfd6ca9e4187db64ac9f6cb
Daria <Tr3deprinter@yandex.ru>
2022-09-05 19:24:15 +0300

    qdf

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ readSceneTrue.on('callback_query', async (ctx) => {
           console.error(e);
       }
       }
-      await ctx.reply (`${r.link}`)
+      await ctx.reply (`${r.smile} ${r.link}`)
     }
   const row = await storybl.findOne({where: {
     linid: ctx.wizard.state.data.readSceneTrue,

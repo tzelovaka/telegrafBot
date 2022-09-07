92b881d3ef7d9c9fcbc9e53fe9680adf371408b2
Daria <Tr3deprinter@yandex.ru>
2022-09-07 22:17:48 +0300

    qdf

--- index.js
+++ index.js
@@ -27,11 +27,10 @@ try {
 
 
 
-bot.start (async (ctx) =>{ 
-  if (ctx.message.from.is_bot = true){
+bot.start ( (ctx) =>
+  /*if (ctx.message.from.is_bot = true){
     await ctx.telegram.kickChatMember(ctx.chat.id, ctx.message.from.id)
-  }
-  await ctx.reply(`Здравствуйте, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}!`)})
+  }*/ ctx.reply(`Здравствуйте, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}!`))
 
 
 

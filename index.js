48ac5aafd987c66ea87aff1863d3e5fb384af00c
Daria <Tr3deprinter@yandex.ru>
2023-11-02 12:33:38 +0300

    make it better

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ searchChoiceScene.on('text', async (ctx) => {
       action: 'filter'}))],
     ])
   );
-  await ctx.replyWithHTML('Редактор для создания интерактива<br />⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇');
+  await ctx.reply('⬇⬇⬇Редактор для создания интерактива⬇⬇⬇');
 } catch(e){
   await ctx.reply('⚠Ошибка!');
   return ctx.scene.leave()

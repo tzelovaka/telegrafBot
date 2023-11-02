5004db37fbf5e6d2096aaf62219fca4dad5659a6
Daria <Tr3deprinter@yandex.ru>
2023-11-02 14:35:05 +0300

    make it better

--- messages.js
+++ messages.js
@@ -2,7 +2,7 @@ const sequelize = require('./db')
 const {DataTypes} = require('sequelize')
 
 const message = sequelize.define ('message', {
-    authId: {type: DataTypes.BIGINT, unique: true},
+    authId: {type: DataTypes.BIGINT},
     message_id: {type: DataTypes.BIGINT, unique: true},
 })
 

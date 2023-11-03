efb41719876fb359277f2e996a64c82fa996eb09
Daria <Tr3deprinter@yandex.ru>
2023-11-03 10:54:27 +0300

    make it better

---
+++ migrations/20231103074741-modify_stories_add_new_fields.js
@@ -0,0 +1,34 @@
+'use strict';
+const {DataTypes} = require('sequelize')
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.addColumn(
+        'stories', // table name
+        'spam', // new field name
+        {
+          type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+        },
+      ),
+      queryInterface.addColumn(
+        'stories',
+        'verification',
+        {
+          type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+        },
+      ),
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.removeColumn('stories', 'spam'),
+      queryInterface.removeColumn('stories', 'verification'),
+    ]);
+  }
+};

357aa24bd256337ddc22cc76b9010c1849c8d770
Daria <Tr3deprinter@yandex.ru>
2023-11-04 19:09:34 +0300

    make it better

--- index.js
+++ index.js
@@ -759,8 +759,8 @@ try{
   spamScene.on('text', async (ctx) => {
     try{
     ctx.wizard.state.data.spamScene = ctx.message.text;
-    const st = await story.findOne({where:{id: BigInt(ctx.wizard.state.data.spamScene)}})
-    st.spam = true
+    let st = await story.findOne({where:{id: BigInt(ctx.wizard.state.data.spamScene)}})
+    st.spam = !st.spam
     await st.save()
     let msg = await ctx.reply('Типа кинули в спам')
     await messages.create({authId: `${msg.chat.id}`, message_id: `${msg.message_id}`})
